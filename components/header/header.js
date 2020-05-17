41ffcdd6e53d850f693f59f0a2aa05efbb3441e3
AndriiShnurkov <Shans12.84@ukr.net>
2020-05-17 17:58:27 +0300

    added cart

--- components/header/header.js
+++ components/header/header.js
@@ -1,13 +1,18 @@
+import { createMarkup, createNewsMarkup } from '../pages/page.js';
+import { games, programs, news } from '../../data.js';
 
 const header = () => {
-
     const navigationList = document.querySelector('.navigationList'); //ul
     const userProfile = document.querySelector('.userProfile');//div
+    const listItems = document.querySelector('.listItems'); //ul
+    const cartForModal = document.querySelector('.cartForModal');
+
     const userName = document.querySelector('.userName'); //span
     const pageName = document.querySelector('.pageName'); //p
 
     const currentActiveLink = navigationList.querySelector('.activeNavLink');
-    pageName.textContent = currentActiveLink.dataset.page; 
+    // pageName.textContent = currentActiveLink.dataset.page;
+    listItems.innerHTML = createMarkup(games);
 
 
     const setActiveLink = (event) => {
@@ -15,18 +20,30 @@ const header = () => {
             const currentActiveLink = navigationList.querySelector('.activeNavLink');
             currentActiveLink && currentActiveLink.classList.remove('activeNavLink');
             event.target.classList.add('activeNavLink');
-            pageName.textContent = event.target.dataset.page; 
+            // pageName.textContent = event.target.dataset.page;
+            listItems.innerHTML = createMarkup(games);
         }
 
-        if (event.target.dataset.page === 'login') {
-            event.target.textContent = 'Log out'.toUpperCase();
-            event.target.dataset.page = 'logout';
-            setActiveUser();
-        } else if (event.target.dataset.page === 'logout') {
-            event.target.textContent = 'Login'.toUpperCase();
-            event.target.dataset.page = 'login';
-            setActiveUser();
+        switch (event.target.dataset.page) {
+            case 'games':
+                listItems.innerHTML = createMarkup(games);
+                break;
+            case 'programs':
+                listItems.innerHTML = createMarkup(programs);
+                break;
+            case 'news':
+                listItems.innerHTML = createNewsMarkup(news);
+                break;
+
+            default:
+                break;
         }
+
+
+
+
+
+
     }
 
     const setActiveUser = () => {
@@ -37,8 +54,29 @@ const header = () => {
         (userName.textContent !== 'Andrii') ? userName.textContent = 'Andrii' : userName.textContent = 'Guest';
     }
 
+    const openOrder = (event) => {
+        const instance = basicLightbox.create(`
+        <div class="modal">
+            
+            <div class="cartWindow">
+            <p>All products in cart: 2</p>
+            <p>Total price: 1200</p>
+            
+            </div>
+            <a>Close</a>
+        </div>
+    `, {
+            onShow: (instance) => {
+                instance.element().querySelector('a').onclick = instance.close
+            }
+        })
+
+        instance.show();
+    }
+
     navigationList.addEventListener('click', setActiveLink); //ul
     userProfile.addEventListener('click', setActiveUser); //div
+    cartForModal.addEventListener('click', openOrder);
 
 
 
---
+++ components/pages/page.js
@@ -0,0 +1,43 @@
+ const createItemMarkup = (item) => {
+    return `
+    <li class="listItem">
+    <h2 class="listItemTitle">${item.title}</h2>
+
+    <div class="listItemImgContainer">
+        <img src=${item.url} alt=${item.title} class="listItemImg"/>
+    </div>
+    <p class="listItemDescription"> <b>Description: </b>${item.description}</p>
+    <div class="order">
+        <p class="listItemPrice"><b>Price: </b>${item.price} UAH</p>
+        <div class="listItemCart">
+            <img src="../../assets/cart.png" alt="cartImage" class="listItemCartIMG"/>
+        </div>
+    </div>
+    </li>
+    `
+}
+
+export const createMarkup = (array) => {
+    let markup = '';
+    for (const item of array) {
+        markup = markup + createItemMarkup(item)
+    }
+    return markup;
+}
+
+const createItemNews = (item) => {
+    return `
+    <li class="listItem">
+    <h2 class="listItemTitle">${item.title}</h2>
+    <p class="listItemDescription">${item.content}</p>
+    </li>
+    `
+}
+
+export const createNewsMarkup = (array) => {
+    let markup = '';
+    for (const item of array) {
+        markup = markup + createItemNews(item)
+    }
+    return markup;
+}
